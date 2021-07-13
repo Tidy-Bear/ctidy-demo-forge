ebfedabfb362517d8d93c71b7ff5eb1994362589
Tidy-Bear <ChenTidyBear@outlook.com>
2021-07-13 16:38:01 +0800

    structure/layout optimized - add: vcs ignore local dir 'mcmodsrepo' - change: comment some init callback unnecessary currently - change: move data output from 'src/generated/resources/' to 'src/main/data/'
    Signed-off-by: Tidy-Bear <ChenTidyBear@outlook.com>

--- src/main/java/me/ctidy/minecraft/mod/forge/demo/CtidyDemo.java
+++ src/main/java/me/ctidy/minecraft/mod/forge/demo/CtidyDemo.java
@@ -1,22 +1,11 @@
 package me.ctidy.minecraft.mod.forge.demo;
 
-import net.minecraft.block.Blocks;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.stream.Collectors;
-
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(CtidyDemo.MODID)
 public class CtidyDemo {
 
@@ -25,40 +14,27 @@ public class CtidyDemo {
 
     public CtidyDemo() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::commonSetup);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
-
-        MinecraftForge.EVENT_BUS.register(this);
+        //FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+        //FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+        //FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
-        // some preinit code
-        LOGGER.info("HELLO FROM PREINIT");
-        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+        LOGGER.info("HELLO FROM PRE-INIT");
     }
 
-    private void clientSetup(final FMLClientSetupEvent event) {
-        // do something that can only be done on the client
-        LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().options);
-    }
+//    private void clientSetup(final FMLClientSetupEvent event) {
+//        LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().options);
+//    }
 
-    private void enqueueIMC(final InterModEnqueueEvent event) {
-        // some example code to dispatch IMC to another mod
-        InterModComms.sendTo(MODID, "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
-    }
+//    private void enqueueIMC(final InterModEnqueueEvent event) {
+//        InterModComms.sendTo(MODID, "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
+//    }
 
-    private void processIMC(final InterModProcessEvent event) {
-        // some example code to receive and process InterModComms from other mods
-        LOGGER.info("Got IMC {}", event.getIMCStream().
-                map(m->m.getMessageSupplier().get()).
-                collect(Collectors.toList()));
-    }
-
-    @SubscribeEvent
-    public void onServerStarting(FMLServerStartingEvent event) {
-        // do something when the server starts
-        LOGGER.info("HELLO from server starting");
-    }
+//    private void processIMC(final InterModProcessEvent event) {
+//        LOGGER.info("Got IMC {}", event.getIMCStream().
+//                map(m->m.getMessageSupplier().get()).
+//                collect(Collectors.toList()));
+//    }
 
 }
